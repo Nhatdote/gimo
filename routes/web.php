a5e1e3f8771ac676447d78404e3c565efeb83d44
Nguyen Duc Nhat <40618849+nhat1379@users.noreply.github.com>
2022-03-20 00:16:17 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 12345;
 });
 
 Route::get('/users', [Controller::class, 'users'])->name('users');
